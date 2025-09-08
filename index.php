4c2b0cd28fdb6dde7414d2c6101b2c8b4de2ab4c
rostyle <rostyle95@gmail.com>
2025-09-08 23:29:13 +0900

    アイコン削除

--- index.php
+++ index.php
@@ -126,7 +126,7 @@ include 'includes/header.php';
                                 <svg class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
-                                🔍 検索する
+                                検索する
                             </button>
                             <div id="search-help" class="sr-only">クリエイターの名前、スキル、専門分野で検索できます</div>
                         </div>
